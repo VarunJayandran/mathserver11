3ca38bdd87af9fdd2a5103368750f634ae0a8e72
VarunJayandran <varunjayandran@gmail.com>
2026-05-31 21:11:40 +0530

    Success

---
+++ varun/mathapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def gst_bill(request):
+    total = None
+
+    if request.method == 'POST':
+        price = float(request.POST['price'])
+        gst = float(request.POST['gst'])
+
+        total = price + (price * gst / 100)
+
+    return render(request, 'mathapp/math.html', {'total': total})
